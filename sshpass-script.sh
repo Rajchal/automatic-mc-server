581debad8fe474f51219a4b09bc630e9fdf0ec3a
Rajchal <rajchalanjal1@gmail.com>
2025-04-26 22:42:06 +0545

    ssh pass improv:

--- sshpass-script.sh
+++ sshpass-script.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
 # === Settings ===
-BACKUP_FOLDER="/opt/minecraft"     # <-- Your server folder
-BACKUP_NAME="mc-backup-$(date +%Y-%m-%d_%H-%M-%S).tar.gz"
-BACKUP_PATH="/tmp/$BACKUP_NAME"
+WORLD_FILE="your world file"
+BACKUP_TAR="backup to send tar"
 
-REMOTE_USER="backupuser"
+REMOTE_USER="root"
 REMOTE_SERVER="your.backup.server.ip"
 REMOTE_PASSWORD="yourpassword"
 REMOTE_PATH="/path/on/backup/server"
 
 # === Create Backup ===
 echo "[*] Creating backup..."
-tar -czvf "$BACKUP_PATH" "$BACKUP_FOLDER"
 
 # === Send Backup ===
 echo "[*] Sending backup to remote server..."
-sshpass -p "$REMOTE_PASSWORD" scp "$BACKUP_PATH" "$REMOTE_USER@$REMOTE_SERVER:$REMOTE_PATH"
+sshpass -p "$REMOTE_PASSWORD" tar -czvf "$WORLD_FILE" "$BACKUP_TAR"
 
 # === Cleanup ===
 echo "[*] Cleaning up local backup..."
