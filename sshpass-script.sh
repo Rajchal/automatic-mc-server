84ff83d2fd3add0bb50a2725a3003f4d542fc8a2
Rajchal <rajchalanjal1@gmail.com>
2025-04-26 22:45:34 +0545

    tweak to mine

--- sshpass-script.sh
+++ sshpass-script.sh
@@ -7,18 +7,11 @@ BACKUP_TAR="backup to send tar"
 REMOTE_USER="root"
 REMOTE_SERVER="your.backup.server.ip"
 REMOTE_PASSWORD="yourpassword"
-REMOTE_PATH="/path/on/backup/server"
 
-# === Create Backup ===
-echo "[*] Creating backup..."
 
-# === Send Backup ===
-echo "[*] Sending backup to remote server..."
 sshpass -p "$REMOTE_PASSWORD" tar -czvf "$WORLD_FILE" "$BACKUP_TAR"
 
-# === Cleanup ===
-echo "[*] Cleaning up local backup..."
-rm "$BACKUP_PATH"
+sshpass -p "$REMOTE_PASSWORD" rsync -avz "$REMOTE_USER"@"$REMOTE_SERVER":"BACKUP_TAR" "/minecraft/backup"
 
-echo "[+] Backup completed successfully!"
+
 
