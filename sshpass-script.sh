9d70afb23863d50f727c392c5c86020e07313563
Rajchal <rajchalanjal1@gmail.com>
2025-04-27 16:18:39 +0545

    using private key besty

--- sshpass-script.sh
+++ sshpass-script.sh
@@ -6,14 +6,13 @@ BACKUP_TAR="backup to send tar"
 
 REMOTE_USER="root"
 REMOTE_SERVER="your.backup.server.ip"
-REMOTE_PASSWORD="yourpassword"
-
+PRIVATE_KEY="/path/to/your/private/key"
 
 # Create a tarball of the world file
-sshpass -p "$REMOTE_PASSWORD" tar -czvf "$BACKUP_TAR" "$WORLD_FILE"
+tar -czvf "$BACKUP_TAR" "$WORLD_FILE"
 
 # Sync the tarball to the remote server
-sshpass -p "$REMOTE_PASSWORD" rsync -avz "$BACKUP_TAR" "$REMOTE_USER"@"$REMOTE_SERVER":"/minecraft/backup"
+rsync -avz -e "ssh -i $PRIVATE_KEY" "$BACKUP_TAR" "$REMOTE_USER"@"$REMOTE_SERVER":"/minecraft/backup"
 
 
 
